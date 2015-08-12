bfaf4a62ddef4fd0ab81badf73a2f58933ff6ffb
janieltec <fjo@cin.ufpe.br>
2015-08-12 01:43:59 -0300

    Atualização 008

--- grails-app/controllers/teste02/Teste03Controller.groovy
+++ grails-app/controllers/teste02/Teste03Controller.groovy
@@ -2,5 +2,6 @@ package teste02
 
 class Teste03Controller {
 	def scaffold = Teste03
+	
    // def index() { }
 }
---
+++ grails-app/controllers/teste02/Teste05Controller.groovy
@@ -0,0 +1,7 @@
+package teste02
+
+class Teste05Controller {
+	def scaffold = Teste05
+
+   // def index() { }
+}
