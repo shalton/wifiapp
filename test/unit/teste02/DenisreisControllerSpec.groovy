e914c2a94f302c198c3803a3509b0235696d5698
denisreis <dpcr@cin.ufpr.br>
2015-08-12 21:42:19 -0300

    denis controller

---
+++ test/unit/teste02/DenisreisControllerSpec.groovy
@@ -0,0 +1,20 @@
+package teste02
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(DenisreisController)
+class DenisreisControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
